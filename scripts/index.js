01ec6d702e553788a8cf4f297b9885203873f057
Torremolinos <adrian_2054@hotmail.com>
2023-10-07 23:51:25 +0200

    Añadiendo css a la calculadora preparando JS

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+/**
+ * @author Adrian Iglesias Riño.
+ * Github: https://github.com/
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    let num = document.ElementById("buttons").value;
+    let display = document.getElementById("display").value;
+   
+    const ingresarNumero=(num)=>{
+       
+    }
+   
+   });
